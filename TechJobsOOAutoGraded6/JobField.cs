8d3f866d906189e634f19076891949c8df7e7f82
lydiaqd <75919027+lydiaqd@users.noreply.github.com>
2023-10-04 19:01:59 -0500

    Final Commit

--- TechJobsOOAutoGraded6/JobField.cs
+++ TechJobsOOAutoGraded6/JobField.cs
@@ -3,7 +3,7 @@ namespace TechJobsOOAutoGraded6
 {
 	public abstract class JobField ///Id
 	{
-        public int Id { get; set; }
+        public int Id { get; }
         private static int nextId = 1;
         public string Value { get; set; }
 
